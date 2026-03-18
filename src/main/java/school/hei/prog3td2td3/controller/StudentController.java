35c8bce1904bcffbd438ee90da74ff7d911bfaa0
Andraina <hei.andraina.4@gmail.com>
2026-03-18 22:37:36 +0300

    feat: add StudentController

---
+++ src/main/java/school/hei/prog3td2td3/controller/StudentController.java
@@ -0,0 +1,47 @@
+package school.hei.prog3td2td3.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.hei.prog3td2td3.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class StudentController {
+    private final List<Student> students = new ArrayList<>();
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam String name) {
+        return "Welcome " + name;
+    }
+
+    @PostMapping("/students")
+    public String addStudents(@RequestBody List<Student> newStudents) {
+
+        students.addAll(newStudents);
+
+        return students.stream()
+                .map(s -> s.getFirstName() + " " + s.getLastName())
+                .collect(Collectors.joining(", "));
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<String> getStudents(
+            @RequestHeader(value = "Accept", defaultValue = "text/plain") String accept) {
+
+        if (accept.equals("text/plain")) {
+            String names = students.stream()
+                    .map(s -> s.getFirstName() + " " + s.getLastName())
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity.ok(names);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.UNSUPPORTED_MEDIA_TYPE) // 415
+                .body("Format non supporté");
+    }
+}
