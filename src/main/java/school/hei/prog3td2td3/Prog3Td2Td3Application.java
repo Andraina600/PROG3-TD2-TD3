e718f74264043a78b7ba6710924987f009343f9f
Andraina <hei.andraina.4@gmail.com>
2026-03-18 21:07:00 +0300

    first commit

---
+++ src/main/java/school/hei/prog3td2td3/Prog3Td2Td3Application.java
@@ -0,0 +1,13 @@
+package school.hei.prog3td2td3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3Td2Td3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3Td2Td3Application.class, args);
+    }
+
+}
---
+++ src/test/java/school/hei/prog3td2td3/Prog3Td2Td3ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.prog3td2td3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3Td2Td3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
