4cae533243267488e72235053c91effe78b497c2
Andraina <hei.andraina.4@gmail.com>
2026-03-25 19:45:05 +0300

    feat: implementation of the code in BadRequestException

--- src/main/java/school/hei/prog3td2td3/exception/BadRequestException.java
+++ src/main/java/school/hei/prog3td2td3/exception/BadRequestException.java
@@ -1,4 +1,7 @@
 package school.hei.prog3td2td3.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
