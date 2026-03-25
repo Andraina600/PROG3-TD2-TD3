69591d3e9d3f123899a9f07d4ebdb11b337c3737
Andraina <hei.andraina.4@gmail.com>
2026-03-25 19:47:02 +0300

    feat: implementation of the code in StudentService

--- src/main/java/school/hei/prog3td2td3/service/StudentService.java
+++ src/main/java/school/hei/prog3td2td3/service/StudentService.java
@@ -1,4 +1,25 @@
 package school.hei.prog3td2td3.service;
 
+import school.hei.prog3td2td3.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StudentService {
+    private final List<Student> students = new ArrayList<>();
+
+    public void addAll(List<Student> newStudents) {
+        students.addAll(newStudents);
+    }
+
+    public List<Student> getAll() {
+        return students;
+    }
+
+    public String getAllAsString() {
+        return students.stream()
+                .map(s -> s.getFirstName() + " " + s.getLastName())
+                .collect(Collectors.joining(", "));
+    }
 }
