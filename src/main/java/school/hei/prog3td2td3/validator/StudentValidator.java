90a37c6afe2a1d39d1d4801794420f492fae014f
Andraina <hei.andraina.4@gmail.com>
2026-03-25 19:46:08 +0300

    feat: implementation of the code in StudentValidator

--- src/main/java/school/hei/prog3td2td3/validator/StudentValidator.java
+++ src/main/java/school/hei/prog3td2td3/validator/StudentValidator.java
@@ -1,4 +1,23 @@
 package school.hei.prog3td2td3.validator;
 
+import school.hei.prog3td2td3.exception.BadRequestException;
+import school.hei.prog3td2td3.model.Student;
+
+import java.util.List;
+
 public class StudentValidator {
+    public void validate(List<Student> students) {
+        for (Student s : students) {
+            if (s.getFirstName() == null || s.getFirstName().isBlank()) {
+                throw new BadRequestException("Le prénom est obligatoire");
+            }
+            if (s.getLastName() == null || s.getLastName().isBlank()) {
+                throw new BadRequestException("Le nom est obligatoire");
+            }
+            if (s.getReference() == null || s.getReference().isBlank()) {
+                throw new BadRequestException("La référence est obligatoire");
+            }
+        }
+    }
+
 }
